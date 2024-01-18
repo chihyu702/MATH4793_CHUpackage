e367e14ad30f633b9f61dab107aeba0391a1ca7f
chihyu <chih.yu.chu-1@ou.edu>
2024-01-18 14:11:37 -0600

    initial commit

---
+++ R/mysquare.R
@@ -0,0 +1,11 @@
+#' Basic square funtion
+#'
+#' @param x  a vector of quantitative date
+#'
+#' @return the square of the input vector
+#' @export
+#'
+#' @examples
+mysquare <- function (x){
+  x^2
+}
